b99ae65ecc98daf86872f6919b90d3b0fc135056
xtw007@outlook.com <xtw719780103+>
2022-09-26 13:11:28 +0800

    增加基类

---
+++ library_viewbinding/src/main/java/com/framework/viewbinding/base/BaseActivity.kt
@@ -0,0 +1,38 @@
+package com.framework.viewbinding.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
+/**
+ * @author: xiaxueyi
+ * @date: 2022-09-23
+ * @time: 15:46
+ * @说明:
+ */
+abstract class BaseActivity<T : ViewBinding> : AppCompatActivity() {
+    open lateinit var mViewBinding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val superclass = javaClass.genericSuperclass
+        if (superclass != null) {
+            val aClass = (superclass as ParameterizedType).actualTypeArguments[0] as Class<*>
+            try {
+                val method = aClass.getDeclaredMethod("inflate", LayoutInflater::class.java)
+                mViewBinding = method.invoke(null, layoutInflater) as T
+                setContentView(mViewBinding.root)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        initView(window.decorView, savedInstanceState)
+    }
+
+    /**
+     * 入口函数
+     */
+    abstract fun initView(rootView: View, savedInstanceState: Bundle?)
+}
---
+++ library_viewbinding/src/main/java/com/framework/viewbinding/base/BaseFragment.kt
@@ -0,0 +1,52 @@
+package com.framework.viewbinding.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import androidx.fragment.app.Fragment
+import java.lang.Exception
+import java.lang.reflect.ParameterizedType
+/**
+ * @author: xiaxueyi
+ * @date: 2022-09-23
+ * @time: 16:20
+ * @说明:
+ */
+abstract class BaseFragment<T : ViewBinding> : Fragment() {
+    open lateinit var mViewBinding: T
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val superclass = javaClass.genericSuperclass
+        if (superclass != null) {
+            val aClass = (superclass as ParameterizedType).actualTypeArguments[0] as Class<*>
+            try {
+                val method = aClass.getDeclaredMethod(
+                    "inflate",
+                    LayoutInflater::class.java,
+                    ViewGroup::class.java,
+                    Boolean::class.javaPrimitiveType
+                )
+                mViewBinding = method.invoke(null, layoutInflater, container, false) as T
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return mViewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView(mViewBinding.root, savedInstanceState)
+    }
+
+    /**
+     * 入口函数
+     */
+    abstract fun initView(rootView: View?, savedInstanceState: Bundle?)
+}

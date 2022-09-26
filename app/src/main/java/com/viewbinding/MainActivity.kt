a14bc82916a708265f0703bdc3d59997a5fe3fb0
xtw007@outlook.com <xtw719780103+>
2022-09-26 14:10:20 +0800

    增加

--- app/src/main/java/com/viewbinding/MainActivity.kt
+++ app/src/main/java/com/viewbinding/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.viewbinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.framework.viewbinding.base.BaseActivity
+import com.viewbinding.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+
+
+    override fun initView(rootView: View, savedInstanceState: Bundle?) {
+        mViewBinding.title.text="不用findViewById 了"
     }
 }
